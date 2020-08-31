589d3f6d648a81614a04ecf8f5b8cbe961e572b4
Maurice Eberle <Maurice.Eberle@hauertmann.net>
2020-08-31 15:09:20 +0200

    fertig;

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,27 +29,108 @@ namespace ToDoListe
 
         public bool IsChecked { get; set; }
         public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IsChecked.ToString()}~{Text}";
+        }
     }
     public partial class MainWindow : Window
     {
-        public List<Listenobjekt> listenObjekte = new List<Listenobjekt>()
+        public List<Listenobjekt> ListenObjekte = new List<Listenobjekt>()
         {
-            new Listenobjekt(false, "Blablabla")               
+
         };
+        public string path = @"..\..\Speicher\Speicher.txt";
         public MainWindow()
         {
             InitializeComponent();
-
-            ToDoBox.ItemsSource = listenObjekte;
+            GetToDos();
+            ToDoBox.ItemsSource = ListenObjekte;
         }
 
         private void btn_add_Click(object sender, RoutedEventArgs e)
         {
-            string input = text_input.Text;
-            var newListenobjekt = new Listenobjekt(false, input);
-            listenObjekte.Add(newListenobjekt);
-            ToDoBox.Items.Refresh();
+            if (text_input.Text.Length > 3)
+            {
+                string input = text_input.Text;
+                var newListenobjekt = new Listenobjekt(false, input);
+                ListenObjekte.Add(newListenobjekt);
+                text_input.Text = "";
+                Update();
+
+            }
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = ToDoBox.SelectedIndex;
+
+            if (index != -1)
+            {
+                ListenObjekte.RemoveAt(index);
+                Update();
+            }
+
+        }
+
+        private void btn_deleteAll_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (int i = ListenObjekte.Count() - 1; i >= 0; i--)
+            {
+                if (ListenObjekte[i].IsChecked)
+                {
+                    ListenObjekte.RemoveAt(i);
+                }
+            }
+            Update();
+        }
+
+        private void btn_change_Click(object sender, RoutedEventArgs e)
+        {
+            int index = ToDoBox.SelectedIndex;
+            if (index != -1)
+            {
+                ListenObjekte[index].Text = text_input.Text;
+            }
             text_input.Text = "";
+            Update();
+        }
+        private void Update()
+        {
+            ToDoBox.Items.Refresh();
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                foreach (var listenObjekt in ListenObjekte)
+                {
+                    sw.WriteLine(listenObjekt.ToString());
+                }
+            }
+        }
+
+        private void GetToDos()
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string toDoContent;
+                while ((toDoContent = sr.ReadLine()) != null)
+                {
+                    string[] newList = toDoContent.Split('~');
+                    bool isChecked = Convert.ToBoolean(newList[0]);
+                    var listenObjekt = new Listenobjekt(isChecked, newList[1]);
+                    ListenObjekte.Add(listenObjekt);
+                }
+            }
+        }
+
+        private void ToDoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ToDoBox.SelectedIndex;
+            if (index != -1)
+            {
+                text_input.Text = ListenObjekte[index].Text;
+            }
         }
     }
 }
